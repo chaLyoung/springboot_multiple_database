ece1242d8eb45d311d9a3165ae8c6faf62cf8b8e
chaLyoung <dreamlovinghigh57@gmail.com>
2022-10-27 19:15:54 +0900

    init basic springboot

---
+++ src/main/java/com/dev/springboot_multiple_database/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dev.springboot_multiple_database;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootMultipleDatabaseApplication.class);
+    }
+
+}
---
+++ src/main/java/com/dev/springboot_multiple_database/SpringbootMultipleDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.dev.springboot_multiple_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultipleDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultipleDatabaseApplication.class, args);
+    }
+
+}
