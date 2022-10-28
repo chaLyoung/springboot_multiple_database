d73d76817a4e7c7af7bd332bb1b3cffbb85073fa
chaLyoung <dreamlovinghigh57@gmail.com>
2022-10-28 15:15:20 +0900

    complete make multiple database config source

--- src/main/java/com/dev/springboot_multiple_database/config/MyBatisPrimaryConfiguration.java
+++ src/main/java/com/dev/springboot_multiple_database/config/MyBatisPrimaryConfiguration.java
@@ -1,12 +1,41 @@
 package com.dev.springboot_multiple_database.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.session.jdbc.config.annotation.SpringSessionDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 @MapperScan(value="com.dev.springboot_multiple_database.mapper.database1", sqlSessionFactoryRef = "primarySqlSessionFactory")
 @EnableTransactionManagement
 public class MyBatisPrimaryConfiguration {
+    @Primary
+    @Bean(name = "primarySqlSessionFactory")
+    @SpringSessionDataSource
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("primaryDataSource") DataSource dataSource, ApplicationContext applicationContext) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:mybatis/mapper/database1/*.xml"));
+
+        SqlSessionFactory factory = sqlSessionFactoryBean.getObject();
+        factory.getConfiguration().setMapUnderscoreToCamelCase(true);
+
+        return factory;
+    }
+
+    @Primary
+    @Bean(name = "primarySqlSessionTemplate")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("primarySqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 }
--- src/main/java/com/dev/springboot_multiple_database/config/MyBatisSecondaryConfiguration.java
+++ src/main/java/com/dev/springboot_multiple_database/config/MyBatisSecondaryConfiguration.java
@@ -1,12 +1,38 @@
 package com.dev.springboot_multiple_database.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.session.jdbc.config.annotation.SpringSessionDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 @MapperScan(value="com.dev.springboot_multiple_database.mapper.database2", sqlSessionFactoryRef = "secondarySqlSessionFactory")
 @EnableTransactionManagement
 public class MyBatisSecondaryConfiguration {
+    @Bean(name = "secondarySqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("secondaryDataSource") DataSource dataSource, ApplicationContext applicationContext) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:mybatis/mapper/database2/*.xml"));
+
+        SqlSessionFactory factory = sqlSessionFactoryBean.getObject();
+        factory.getConfiguration().setMapUnderscoreToCamelCase(true);
+
+        return factory;
+    }
+
+    @Bean(name = "secondarySqlSessionTemplate")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("secondarySqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 }
